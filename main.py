e0746f6b49261d1a64a3e681acd4cfd81f9af435
angelnath <adedirannath@gmail.com>
2026-05-02 16:50:54 +0100

    update 1

--- main.py
+++ main.py
@@ -5,7 +5,20 @@ from fastapi import FastAPI
 # creating an instance of fastapi app
 app = FastAPI()
 
-Windsurf Reflector | Explain | Generate Docstrings |
+# Windsurf Reflector | Explain | Generate Docstrings
 @app.get("/") #Define a route for the root URL
 def home():
     return {"message": "Welcome to the FastAPI application!"}
+
+# Check about status
+@app.get("/about")
+def about_info():
+    return {
+        "course": "Python Fullstack Development by Analytic Sages",
+        "instructor": "Defi__Josh",
+        "description": "This course covers how to build a fullstack app using python and FastAPI"
+        }
+
+@app.get("/csv")
+def csv():
+    csv_content
